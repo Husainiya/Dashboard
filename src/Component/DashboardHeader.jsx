ab7de1f4ef8c46d86a739fa01cfa456292a19e16
Husainiya <hsynyhsn1122@gmail.com>
2025-03-24 20:36:23 +0530

    StatsCard component

--- src/Component/DashboardHeader.jsx
+++ src/Component/DashboardHeader.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+import "./dashboardHeader.css"; 
 
 const DashboardHeader = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Row className="dashboard-header mb-3">
+      <Col>
+      <Card className="text-center" style={{ backgroundColor: '#afd5f5ab' }}>
+          <h5>Daily Active Users</h5>
+          <h3>1051</h3>
+          <small>18 May 2025</small>
+        </Card>
+      </Col>
+      <Col>
+      <Card className="text-center" style={{ backgroundColor: '#afd5f5ab' }}>
+          <h5>Monthly Active Users</h5>
+          <h3>1051</h3>
+          <small>18 May 2025</small>
+        </Card>
+      </Col>
+      <Col>
+      <Card className="text-center" style={{ backgroundColor: '#afd5f5ab' }}>
+          <h5>Daily Time Per Active User</h5>
+          <h3>1051</h3>
+          <small>18 May 2025</small>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 
-export default DashboardHeader
+export default DashboardHeader;
--- src/Component/StatsCard.jsx
+++ src/Component/StatsCard.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import "./statsCard.css"; 
 
-const StatsCard = () => {
+const StatsCard = ({ title, value }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card className="p-3 text-center">
+      <h5>{title}</h5>
+      <h3>{value}</h3>
+      <small>{date}</small>
+    </Card>
+  );
+};
 
-export default StatsCard
+export default StatsCard;
